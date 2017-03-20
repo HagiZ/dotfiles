daad85dee73cca4132a068e449027036f472f1a7
Daniel H <daniel@asball.se>
2017-03-20 15:01:38 +0100

    Primary upload

---
+++ .emacs.d/init.el
@@ -0,0 +1,20 @@
+;; Emacs init.el
+;; daniel(at)hagiz.se
+
+;; Setup the package manager.
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/")
+	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+;; Initialize the package manager.
+(package-initialize)
+
+;; Bootstrap 'use-package'.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Load the settings from 'settings.org'
+(org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
